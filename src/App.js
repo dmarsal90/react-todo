dd351e2112347f2a3a33b5fa8219dfa081401674
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-22 21:06:15 -0400

    Import useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function App() {
+  const [newTaskname, setNewTaskName] = useState();
+
   return (
     <div className="App">
-
-      <input type='text' placeholder='Enter a new task' onChange={(e) => console.log(e)}/>
-      <button onClick={()=>alert('clicked')}><FontAwesomeIcon icon={faPlus}/> Save task</button>
-
+      <input
+        type="text"
+        placeholder="Enter a new task"
+        onChange={(e) => setNewTaskName(e.target.value)}
+      />
+      <button onClick={() => alert(newTaskname)}>
+        <FontAwesomeIcon icon={faPlus} /> Save task
+      </button>
     </div>
   );
 }
