7877805f677ab619c7a213196ca27ae8bbacdf32
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-22 23:57:34 -0400

    Add TaskCreator to App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { TaskCreator } from "./components/TaskCreator";
 
 function App() {
-  const [newTaskname, setNewTaskName] = useState();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    localStorage.setItem('tasks', newTaskname);
-    setNewTaskName('');
-  }
-
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={newTaskname}
-          placeholder="Enter a new task"
-          onChange={(e) => setNewTaskName(e.target.value)}
-        />
-        <button>
-          <FontAwesomeIcon icon={faPlus} /> Save task
-        </button>
-      </form>
+      <TaskCreator />
     </div>
   );
 }
---
+++ src/components/TaskCreator.js
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+export const TaskCreator = () => {
+
+    const [newTaskName, setNewTaskName] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("tasks", newTaskName);
+      setNewTaskName("");
+    };
+    return (
+
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={newTaskName}
+                placeholder="Enter a new task"
+                onChange={(e) => setNewTaskName(e.target.value)}
+            />
+            <button>
+                <FontAwesomeIcon icon={faPlus} /> Save task
+            </button>
+        </form>
+    );
+};
