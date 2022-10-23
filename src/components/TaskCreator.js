1ce9c154f79e953b874d8282c8f43f34a1705dc4
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-23 10:43:02 -0400

    Add props create task to handle submit

--- src/components/TaskCreator.js
+++ src/components/TaskCreator.js
@@ -2,27 +2,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
-export const TaskCreator = () => {
-
-    const [newTaskName, setNewTaskName] = useState();
+export const TaskCreator = (props) => {
+  const [newTaskName, setNewTaskName] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    props.createTask(newTaskName);
     localStorage.setItem("tasks", newTaskName);
-      setNewTaskName("");
-    };
-    return (
-
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={newTaskName}
-                placeholder="Enter a new task"
-                onChange={(e) => setNewTaskName(e.target.value)}
-            />
-            <button>
-                <FontAwesomeIcon icon={faPlus} /> Save task
-            </button>
-        </form>
-    );
+    setNewTaskName("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={newTaskName}
+        placeholder="Enter a new task"
+        onChange={(e) => setNewTaskName(e.target.value)}
+      />
+      <button>
+        <FontAwesomeIcon icon={faPlus} /> Save task
+      </button>
+    </form>
+  );
 };
