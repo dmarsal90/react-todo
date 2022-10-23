5da7bd7802611db26a6cf8bc1cb2e9e432134f3f
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-23 11:53:57 -0400

    Add data to taskItems

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import { TaskCreator } from "./components/TaskCreator";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [tasksItems, setTaskitems] = useState([
-    { name: "Mi primera tarea", done: false },
-    { name: "Mi segunda tarea", done: false },
-    { name: "Mi tercera tarea", done: false },
-  ]);
+  const [tasksItems, setTaskItems] = useState([]);
 
   function createTask(taskName) {
     if (!tasksItems.find((task) => task.name === taskName)) {
-      setTaskitems([...tasksItems, { name: taskName, done: false }]);
+      setTaskItems([...tasksItems, { name: taskName, done: false }]);
     } else {
       alert("The task already exist");
     }
   }
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasksItems));
+    let data = localStorage.getItem("tasks");
+    if (data) {
+      setTaskItems(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasksItems));
   }, [tasksItems]);
 
   return (
