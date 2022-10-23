4bff63ad64341c47a96852edfe085717a8145882
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-23 00:11:24 -0400

    Add table for tasks to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./App.css";
 import { TaskCreator } from "./components/TaskCreator";
+import { useState } from "react";
 
 function App() {
+  const [tasksItems, setTaskitems] = useState([
+    { name: "Mi primera tarea", done: false },
+    { name: "Mi segunda tarea", done: false },
+    { name: "Mi tercera tarea", done: false },
+  ]);
+
   return (
     <div className="App">
       <TaskCreator />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Tasks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasksItems.map((task) => (
+            <tr key={task.name}>
+              <td>{task.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
