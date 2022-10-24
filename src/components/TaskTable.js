28e8b2a167b3025cfbeed446326d8e2733830825
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-24 16:56:01 -0400

    Add filter to taskTableRow const

--- src/components/TaskTable.js
+++ src/components/TaskTable.js
@@ -1,6 +1,17 @@
 import { TaskRow } from "./TaskRow";
 
 export const TaskTable = ({ tasks, toggleTask }) => {
+
+  const taskTableRows = (doneValue) => {
+    {
+      return tasks
+        .filter((task) => task.done === false)
+        .map((task) => (
+          <TaskRow task={task} key={task.name} toggleTask={toggleTask} />
+        ));
+    }
+  };
+
   return (
     <table>
       <thead>
@@ -8,11 +19,7 @@ export const TaskTable = ({ tasks, toggleTask }) => {
           <th>Tasks</th>
         </tr>
       </thead>
-      <tbody>
-        {tasks.map((task) => (
-          <TaskRow task={task} key={task.name} toggleTask={toggleTask} />
-        ))}
-      </tbody>
+      <tbody>{taskTableRows(false)}</tbody>
     </table>
   );
 };
