9cef3e312882a2512e867ac57d4cc078251f82ab
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-24 22:47:45 -0400

    Update TaskTable

--- src/components/TaskTable.js
+++ src/components/TaskTable.js
@@ -1,35 +1,13 @@
 import { TaskRow } from "./TaskRow";
 
-<<<<<<< HEAD
 export const TaskTable = ({ tasks, toggleTask, showCompleted = false }) => {
-
-  const taskTableRows = (doneValue) => {    
-      return (
-        tasks
-          .filter(task => task.done === doneValue)
-          .map(task => (
-            <TaskRow task={task} key={task.name} toggleTask={toggleTask} />
-          ))
-      );
-    }
-  
-=======
-export const TaskTable = ({ tasks, toggleTask }) => {
-
-  const taskTableRows = () => {
-    {
-      return (
-        tasks
-          .filter(task => task.done === false)
-          .map((task) => (
-            <TaskRow task={task} key={task.name} toggleTask={toggleTask} />
-          ))
-      );
-
-    }
-  }
->>>>>>> e917bbea1a8665541e59d99911d914770543ee78
-
+  const taskTableRows = (doneValue) => {
+    return tasks
+      .filter((task) => task.done === doneValue)
+      .map((task) => (
+        <TaskRow task={task} key={task.name} toggleTask={toggleTask} />
+      ));
+  };
   return (
     <table>
       <thead>
@@ -37,13 +15,8 @@ export const TaskTable = ({ tasks, toggleTask }) => {
           <th>Tasks</th>
         </tr>
       </thead>
-<<<<<<< HEAD
+
       <tbody>{taskTableRows(showCompleted)}</tbody>
-=======
-      <tbody>
-        taskTableRows(false);
-      </tbody>
->>>>>>> e917bbea1a8665541e59d99911d914770543ee78
     </table>
   );
 };
