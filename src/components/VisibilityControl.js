3ac1ce6097a341520f7f976eed6404b73aa51c31
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-25 22:48:32 -0400

    Add styles to VisibilityControl.js

--- src/components/VisibilityControl.js
+++ src/components/VisibilityControl.js
@@ -1,20 +1,22 @@
-export const VisibilityControl = ({ setShowCompleted, cleanTasks, isChecked}) => {
-  const handleDelete = () => {
-      if (window.confirm('Are you sure you want to delete it?')) {
-          cleanTasks();
-    }
-  };
+export const VisibilityControl = ({setShowCompleted, cleanTasks, isChecked}) => {
+    const handleDelete = () => {
+        if (window.confirm('Are you sure you want to delete it?')) {
+            cleanTasks();
+        }
+    };
 
-  return (
-    <div>
-      <input
-              type="checkbox"
-              checked={isChecked}
-        onChange={(e) => setShowCompleted(e.target.checked)}
-      />{""}
-      <label>Show tasks done</label>
+    return (
+        <div className="d-flex justify-content-between bg-secondary text-white text-center p-2 border-secondary">
+            <div className="form-check form-switch">
+                <input className="form-check-input"
+                       type="checkbox"
+                       checked={isChecked}
+                       onChange={(e) => setShowCompleted(e.target.checked)}
+                />{""}
+                <label>Show tasks done</label>
+            </div>
 
-      <button onClick={handleDelete}>Clear</button>
-    </div>
-  );
+            <button onClick={handleDelete} className="btn btn-danger btn-sm">Clear</button>
+        </div>
+    );
 };
